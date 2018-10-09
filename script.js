d8590f0d02630aa24d99d4b481c2646fa77d8ee2
Albert K. Nguyen <43667021+AdmiralQuackbar@users.noreply.github.com>
2018-10-08 19:12:26 -0700

    Scroll Edit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function($) {
     
-    
     $("#home").on("click", function() {
         window.scroll({
             top: 0, 
             left: 0, 
             behavior: 'smooth' 
-          });
+        });
+        page = 1
     });
 
     $("#about").on("click", function() {
@@ -14,7 +14,8 @@ $(document).ready(function($) {
             top: 892, 
             left: 0, 
             behavior: 'smooth' 
-          });
+        });
+        page = 2
     });
 
     $("#contact").on("click", function() {
@@ -22,14 +23,18 @@ $(document).ready(function($) {
             top: 1781, 
             left: 0, 
             behavior: 'smooth' 
-          });
+        });
+        page = 3
     });
 
     $(".menu").on("click", function(e) {
         e.preventDefault(); 
     });
-    
-    $(document).on("scroll",function() {
+   
+    var screenPos = 0;
+    var page = 1;
+
+    $(window).on("scroll",function() {
         var scrollTop = $(document).scrollTop()
         var scrollBottom = scrollTop + $(window).height() 
         var pageBottom = $(document).height()
@@ -42,6 +47,61 @@ $(document).ready(function($) {
         $(".intro-section").css("opacity", opacity1)
         $(".contact-section").css("opacity", opacity2)
         $(".footer").css("opacity", opacity3)
+
+        var scrollUp = false;
+        if(screenPos > scrollTop) {
+            scrollUp = true;
+        } else if(screenPos < scrollTop) {
+            scrollUp = false;
+        } else {}
+
+        
+        if(scrollTop < 892 && page == 2 && scrollUp == true) {
+            window.scroll({
+                top: 0, 
+                left: 0, 
+                behavior: 'smooth', 
+            });
+            page = 1
+        }
+ 
+        if(scrollTop > 0 && page == 1 && scrollUp == false) {
+            window.scroll({
+                top: 892, 
+                left: 0, 
+                behavior: 'smooth', 
+            });
+            page = 2
+        }
+
+        if(scrollTop < 1781 && page == 3 && scrollUp == true) {
+            window.scroll({
+                top: 892, 
+                left: 0, 
+                behavior: 'smooth', 
+            });
+            page = 2
+        }
+
+        if(scrollTop > 892 && page == 2 && scrollUp == false) {
+            window.scroll({
+                top: 1781, 
+                left: 0, 
+                behavior: 'smooth', 
+            });
+            page = 3
+        }    
+
+        screenPos = scrollTop;
+
     });
+
+    $(document).keydown(function(event) {
+        $(window).bind("mousewheel DOMMouseScroll", function (event) {
+            if (event.ctrlKey == true) {
+                event.preventDefault();
+            }
+        });
+    }); 
 
 });
