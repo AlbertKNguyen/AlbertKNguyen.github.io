a5d5f9637dcc6202f60c99f064b208e88f1a4f75
Albert K. Nguyen <43667021+AdmiralQuackbar@users.noreply.github.com>
2018-12-18 04:55:07 -0800

    intro update

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ $(document).ready(function($) {
     var screenPos = 0;
     var page = 1;
 
-
-
-
     $("#home").on("click", function(e) {
         window.scroll({
             top: 0, 
@@ -18,7 +15,7 @@ $(document).ready(function($) {
 
     $("#about").on("click", function(e) {
         window.scroll({
-            top: 890, 
+            top: 891, 
             left: 0, 
             behavior: 'smooth' 
         });
@@ -26,18 +23,7 @@ $(document).ready(function($) {
         e.preventDefault(); 
     });
 
-    
-    $(".arrow1").on("click", function(e) {
-        window.scroll({
-            top: 890, 
-            left: 0, 
-            behavior: 'smooth' 
-        });
-        page = 2
-        e.preventDefault(); 
-    });
-
-    $(".arrow2").on("click", function(e) {
+    $("#contact").on("click", function(e) {
         window.scroll({
             top: 1781, 
             left: 0, 
@@ -47,17 +33,6 @@ $(document).ready(function($) {
         e.preventDefault(); 
     });
 
-    $("#contact").on("click", function(e) {
-        window.scroll({
-            top: 1780, 
-            left: 0, 
-            behavior: 'smooth' 
-        });
-        page = 3
-        e.preventDefault(); 
-    });
-
-    /*
     $(window).on("scroll",function() {
         var scrollTop = $(document).scrollTop()
         var scrollBottom = scrollTop + $(window).height() 
@@ -79,8 +54,8 @@ $(document).ready(function($) {
             scrollUp = false;
         } else {}
 
-        
-        if(scrollTop < 890 && page == 2 && scrollUp == true) {
+        /*  single scroll page browser
+        if(scrollTop < 891 && page == 2 && scrollUp == true) {
             window.scroll({
                 top: 0, 
                 left: 0, 
@@ -91,33 +66,33 @@ $(document).ready(function($) {
  
         if(scrollTop > 0 && page == 1 && scrollUp == false) {
             window.scroll({
-                top: 890, 
+                top: 891, 
                 left: 0, 
                 behavior: 'smooth', 
             });
             page = 2 
         }
 
-        if(scrollTop < 1780 && page == 3 && scrollUp == true) {
+        if(scrollTop < 1781 && page == 3 && scrollUp == true) {
             window.scroll({
-                top: 890, 
+                top: 891, 
                 left: 0, 
                 behavior: 'smooth', 
             });
             page = 2
         }
 
-        if(scrollTop > 890 && page == 2 && scrollUp == false) {
+        if(scrollTop > 891 && page == 2 && scrollUp == false) {
             window.scroll({
-                top: 1780, 
+                top: 1781, 
                 left: 0, 
                 behavior: 'smooth', 
             });
             page = 3
         }    
 
-        screenPos = scrollTop;  
+        screenPos = scrollTop;*/
 
-    });   */
+    }); 
 
 });
