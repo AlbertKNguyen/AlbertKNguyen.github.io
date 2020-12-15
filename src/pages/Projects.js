11843c2bc3e01b9c381b9f54ebf2e01d83aed7be
Albert Kenneth Nguyen <albert@sweepenergy.com>
2020-12-15 06:40:38 -0800

    Add Loader & link to current project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,31 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import PageHeader from '../components/PageHeader';
-import { Card } from 'semantic-ui-react'
+import { Card, Loader } from 'semantic-ui-react'
 import axios from 'axios';
 import '../App.css';
 
+const cardStyle = {
+  backgroundColor: '#1E1E1E',
+  marginTop: '-5px',
+  marginLeft: '10px',
+  marginRight: '10px',
+  wordWrap: 'break-word'
+}
 
 function Projects(props) {
   const [repoList, setRepoList] = useState([]);
-  const cardStyle = {
-    backgroundColor: '#1E1E1E',
-    marginTop: '-5px',
-    marginLeft: '10px',
-    marginRight: '10px',
-    wordWrap: 'break-word'
-  }
+  let [loading, setLoading] = useState(false);
 
   useEffect(() => {
     // Update document title
     document.title = 'Albert Nguyen | Projects';
 
-    async function getGithubRepoList() {
+    const getGithubRepoList = async () => {
+      setLoading(true);
       try {
         const result = await axios.get('https://api.github.com/users/AlbertKNguyen/repos');
         setRepoList(result.data);
       } catch (error) {
         console.log('error', error);
       }
+      setLoading(false);
     }
     getGithubRepoList();
   }, []);
@@ -34,16 +37,19 @@ function Projects(props) {
     <div style={props.style}>
       <PageHeader title='Projects' isDesktop={props.isDesktop} />
       <div className='contentScroll'>
-        <h2 className='textStyle'> Projects are still in the works... But feel free to check out my <a href="https://github.com/AlbertKNguyen">Github</a></h2>
+        <h2 className='textStyle'>My current ongoing project: <a href="https://whodelivers.app">https://whodelivers.app</a></h2>
+        <h2 className='textStyle'>My <a href="https://github.com/AlbertKNguyen">Github</a>:</h2>
         <Card.Group centered itemsPerRow={3} style={{ marginTop: '5px' }}>
-          {repoList.map(repo => (
+          {!loading ? repoList.map(repo => (
             <Card key={repo.id} href={'https://github.com/AlbertKNguyen/' + repo.name} style={cardStyle}>
               <Card.Content>
                 <Card.Header style={{ color: '#F4F4F4' }} content={repo.name} />
                 <Card.Description style={{ color: '#D4D4D4' }} content={repo.description} />
               </Card.Content>
             </Card>
-          ))}
+          )) :
+            <Loader active>Github repositories loading...</Loader>
+          }
         </Card.Group>
       </div>
     </div>
